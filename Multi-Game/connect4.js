0c8bdd6eb58bcedbca3bbb29792c03933d1c1441
Davery Valdez <davery15@nmsu.edu>
2024-02-28 17:07:29 -0700

    Almost done

--- Multi-Game/connect4.js
+++ Multi-Game/connect4.js
@@ -4,7 +4,6 @@ CS 382
 Multiplayer-Game
 
 */
-
 var playerRed = "Red";
 var playerYellow = "Yellow";
 var currentPlayer = playerRed;
@@ -39,7 +38,6 @@ function setGame() {
 
         board.push(row);
     }
-    updateTurnDisplay();
 }
 
 function setPiece() {
@@ -66,7 +64,69 @@ function setPiece() {
     r -= 1;
     currentColumn[c] = r;
 
-    updateTurnDisplay();
+    
+checkWin();
+updateTurnDisplay();
+}
+
+function checkWin() {
+    for (let r = 0; r < rows; r++) { //horizontal
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ' ) {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    for (let c = 0; c < columns; c++) { //vertical
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+
+            }
+        }
+    }
+    for (let r = 0; r < rows-3; r++) { //diagonal
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if(board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    let fireworks = document.getElementById("fireworks"); // Get fireworks container
+    if (board[r][c] == playerRed) {
+        winner.innerText = "Red Wins!!";
+        winner.classList.add("winner"); // Add class for animated display
+        fireworks.classList.add("active"); // Activate fireworks animation
+    } else {
+        winner.innerText = "Yellow Wins!!";
+        winner.classList.add("winner"); // Add class for animated display
+        fireworks.classList.add("active"); // Activate fireworks animation
+    }
+    gameOver = true;
 }
 
 function updateTurnDisplay() {
@@ -80,6 +140,12 @@ function resetGame() {
     // Reset variables
     currentPlayer = playerRed;
     gameOver = false;
+    // Clear the winner display and remove animations
+    let winner = document.getElementById("winner");
+    winner.innerText = "";
+    winner.classList.remove("winner");
+    let fireworks = document.getElementById("fireworks");
+    fireworks.classList.remove("active");
     // Reset the game board
     setGame();
 }
